e0ff9d52ffa256bd9bde33bc05c6d987123f9268
hanielrolemberg <victorhanielbusiness@gmail.com>
2024-06-17 01:18:59 -0300

    2 simple examples

--- testmain.py
+++ testmain.py
@@ -1,8 +1,22 @@
 import random
-message = "TEST"
+message = "test"
 
 # Define the DNA encoding and decoding dictionaries
-encoding_dict = {'A': 'AT', 'T': 'UI', 'C': 'CG', 'H': 'HT', 'E': 'EA', 'L': 'LG', 'O': 'OT', 'W': 'WT', 'R': 'RT', 'D': 'DT', ' ': 'SP', 'V': 'VA', 'S': 'SZ', 'B': 'BW', 'I': 'IY'}
+encoding_dict = {
+    'A': 'AT', 'B': 'BW', 'C': 'CG', 'D': 'DT', 'E': 'EA', 'F': 'FT', 'G': 'GT', 'H': 'HT', 'I': 'IY',
+    'J': 'JT', 'K': 'KT', 'L': 'LG', 'M': 'MT', 'N': 'NT', 'O': 'OT', 'P': 'PT', 'Q': 'QT', 'R': 'RT',
+    'S': 'SZ', 'T': 'UI', 'U': 'UY', 'V': 'VA', 'W': 'WT', 'X': 'XT', 'Y': 'YT', 'Z': 'ZT',
+    'a': 'aT', 'b': 'bW', 'c': 'cG', 'd': 'dT', 'e': 'eA', 'f': 'fT', 'g': 'gT', 'h': 'hT', 'i': 'iY',
+    'j': 'jT', 'k': 'kT', 'l': 'lG', 'm': 'mT', 'n': 'nT', 'o': 'oT', 'p': 'pT', 'q': 'qT', 'r': 'rT',
+    's': 'sZ', 't': 'uI', 'u': 'uY', 'v': 'vA', 'w': 'wT', 'x': 'xT', 'y': 'yT', 'z': 'zT',
+    '0': '0T', '1': '1T', '2': '2T', '3': '3T', '4': '4T', '5': '5T', '6': '6T', '7': '7T', '8': '8T', '9': '9T',
+    ' ': 'SP', '!': 'EX', '@': 'AT', '#': 'HS', '$': 'DL', '%': 'PC', '^': 'CR', '&': 'AN', '*': 'AS', '(': 'LP', ')': 'RP',
+    '-': 'DS', '_': 'US', '+': 'PL', '=': 'EQ', '{': 'LB', '}': 'RB', '[': 'LS', ']': 'RS', ':': 'CL', ';': 'SC', '"': 'QT',
+    "'": 'SQ', '<': 'LT', '>': 'GT', ',': 'CM', '.': 'DT', '?': 'QM', '/': 'FS', '\\': 'BS', '|': 'PP'
+}
+
+
+#encoding_dict = {'A': 'AT', 'T': 'UI', 'C': 'CG', 'H': 'HT', 'E': 'EA', 'L': 'LG', 'O': 'OT', 'W': 'WT', 'R': 'RT', 'D': 'DT', ' ': 'SP', 'V': 'VA', 'S': 'SZ', 'B': 'BW', 'I': 'IY'}
 #encoding_dict = {'A': 'AT', 'T': 'TA', 'C': 'CG', 'G': 'GC'}
 decoding_dict = {v: k for k, v in encoding_dict.items()}
 
